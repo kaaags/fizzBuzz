61451c4a71247fd74c6cb8eab9ff8e3836905bed
Sean Kagawa <seankagawa@gmail.com>
2018-02-17 21:15:52 -1000

    compl exercise

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ if the number is a multiple of 3, it will console.log "fizz",
 if the number is a multiple of 5, it will console.log "buzz",  
 if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
 */
+function fizzBuzz(){
+	for(i = 1; i <= 100; i++){
+		if(i % 3 === 0 && i % 5 === 0){
+			console.log("fizzBuzz");
+		}else if(i % 3 === 0){
+			console.log("fizz");
+		}else if(i % 5 === 0){
+			console.log("buzz");
+		}else{
+			console.log(i);
+		}
+	}
+}
+
+fizzBuzz();
